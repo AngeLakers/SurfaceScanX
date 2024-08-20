b032ab8c9597b1b1d8691a23848ca45f6c3d611c
billwu <sanbgi@qq.com>
2024-08-20 11:19:36 +0800

    PCI9850 & LTDMC

---
+++ thridpart/LTDMC/LTDMC-symbols.cpp
@@ -0,0 +1,4 @@
+#include <LTDMC.h>
+#include <new>
+
+struct_PidAdjustData& (struct_PidAdjustData::*_0)(struct_PidAdjustData&&) = &struct_PidAdjustData::operator=;
---
+++ thridpart/PCI9850/PCI9850-symbols.cpp
@@ -0,0 +1,4 @@
+#include <PCI9850.h>
+#include <new>
+
+struct _PCI9850_PARA_INIT& (_PCI9850_PARA_INIT::*_0)(struct _PCI9850_PARA_INIT&&) = &_PCI9850_PARA_INIT::operator=;
